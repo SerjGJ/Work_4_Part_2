00bca0ec895e305f4e0f440937b2fc49da2988ca
Sergey Kozochkin <kozochkin2000@icloud.com>
2023-12-02 18:00:06 +0500

    Part_2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,83 +1,59 @@
-import { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import styles from './app.module.css';
 
+const schema = yup.object().shape({
+	email: yup
+		.string()
+		.matches(/\S+@\S+\.\S+/, 'Некорректный email')
+		.required('Email обязателен для заполнения'),
+	password: yup
+		.string()
+		.min(3, 'Пароль должен содержать минимум 3 символа')
+		.max(20, 'Пароль должен содержать максимум 20 символов')
+		.required('Пароль обязателен для заполнения'),
+	confirmPassword: yup
+		.string()
+		.test('password', 'Пароли не совпадают', function (value) {
+			return this.parent.password === value;
+		})
+		.required('Подтверждение пароля обязательно'),
+});
+
 export const App = () => {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-		confirmPassword: '',
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
 	});
 
-	const [formErrors, setFormErrors] = useState({
-		email: '',
-		password: '',
-		confirmPassword: '',
-	});
-
-	const registerButtonRef = useRef(null);
-
-	const validateEmail = (email) => /\S+@\S+\.\S+/.test(email) || email === '';
-	const validatePassword = (password) => (password.length >= 3 && password.length <= 20) || password === '';
-
-	const validateField = (fieldName, value) => {
-		let error = '';
-
-		if (fieldName === 'email') error = validateEmail(value) ? '' : 'Некорректный email';
-		else if (fieldName === 'password') error = validatePassword(value) ? '' : 'Пароль должен содержать от 3 до 20 символов';
-		else if (fieldName === 'confirmPassword') error = value === formData.password ? '' : 'Пароли не совпадают';
-
-		setFormErrors({ ...formErrors, [fieldName]: error });
+	const onSubmit = (dataForm) => {
+		console.log('Данные формы:', dataForm);
 	};
-
-	const handleInputChange = (fieldName, value) => {
-		setFormData({ ...formData, [fieldName]: value });
-		validateField(fieldName, value);
-		if ((fieldName === 'password' || fieldName === 'confirmPassword') && formData.password === value) setFormErrors({ ...formErrors, confirmPassword: '' });
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (!Object.values(formErrors).every((error) => error === '') || Object.values(formData).some((value) => value === '')) {
-			console.log('Форма содержит ошибки. Пожалуйста, исправьте их.');
-			return;
-		}
-		console.log('Данные формы:', formData);
-	};
-
-	const focusRegisterButton = () => {
-		if (Object.values(formErrors).every((error) => error === '') && Object.values(formData).every((value) => value !== '')) {
-			registerButtonRef.current.focus();
-		}
-	};
-
-	const isFormValid = () =>
-		Object.values(formErrors).every((error) => error === '') && formData.email !== '' && formData.password !== '' && formData.confirmPassword !== '';
-
-	focusRegisterButton();
 
 	return (
 		<div className={styles.app}>
 			<h1>Регистрация</h1>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<div>
 					<label>Email:</label>
-					<input type="email" value={formData.email} onChange={(event) => handleInputChange('email', event.target.value)} />
-					<span>{!validateEmail(formData.email) ? 'Некорректный email' : formErrors.email}</span>
+					<input type="email" {...register('email')} className={errors.email ? 'inputError' : ''} />
+					<span>{errors.email?.message}</span>
 				</div>
 				<div>
-					<label>Придумайте пароль:</label>
-					<input type="password" value={formData.password} onChange={(event) => handleInputChange('password', event.target.value)} />
-					<span>{!validatePassword(formData.password) ? 'Пароль должен содержать от 3 до 20 символов' : formErrors.password}</span>
+					<label>Пароль:</label>
+					<input type="password" {...register('password')} className={errors.password ? 'inputError' : ''} />
+					<span>{errors.password?.message}</span>
 				</div>
 				<div>
-					<label>Повторите пароль:</label>
-					<input type="password" value={formData.confirmPassword} onChange={(event) => handleInputChange('confirmPassword', event.target.value)} />
-					<span>{formErrors.confirmPassword}</span>
+					<label>Подтвердите пароль:</label>
+					<input type="password" {...register('confirmPassword')} className={errors.confirmPassword ? 'inputError' : ''} />
+					<span>{errors.confirmPassword?.message}</span>
 				</div>
-				<button type="submit" disabled={!isFormValid()}>
-					Зарегистрироваться
-				</button>
-				<button ref={registerButtonRef} style={{ display: 'none' }}></button>
+				<button type="submit">Зарегистрироваться</button>
 			</form>
 		</div>
 	);
